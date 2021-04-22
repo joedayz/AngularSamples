99b073e0bb09d8189ea2e5b6b88962a013d6728b
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-21 20:35:07 -0500

    binding to form controls, example: select

--- profesional/04/forms-setup/src/app/stock-inventory/containers/stock-inventory.component.ts
+++ profesional/04/forms-setup/src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {Product} from '../models/product.interface';
 
 
 @Component({
@@ -9,7 +10,7 @@ import {FormArray, FormControl, FormGroup} from '@angular/forms';
       <form [formGroup]="form" (ngSubmit)="onSubmit()">
 
         <stock-branch [parent]="form"></stock-branch>
-        <stock-selector [parent]="form"></stock-selector>
+        <stock-selector [parent]="form" [products]="products"></stock-selector>
         <stock-products [parent]="form"></stock-products>
 
         <div class="stock-inventory__buttons">
@@ -25,6 +26,15 @@ import {FormArray, FormControl, FormGroup} from '@angular/forms';
   `
 })
 export class StockInventoryComponent {
+
+  products: Product[] = [
+    { "id": 1, "price": 2800, "name": "MacBook Pro" },
+    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
+    { "id": 3, "price": 400, "name": "iPod" },
+    { "id": 4, "price": 900, "name": "iPhone" },
+    { "id": 5, "price": 600, "name": "Apple Watch" },
+  ];
+
 
   form = new FormGroup({
     store: new FormGroup({
