2410713e5f5ad6ea92b7dbde1583ee153cfeb950
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-23 20:50:33 -0500

    implementing a ControlValueAccessor

--- profesional/04/forms-setup/src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ profesional/04/forms-setup/src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,8 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
+const COUNTER_CONTROL_ACCESOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef( () => StockCounterComponent),
+  multi: true
+
+}
 
 @Component({
   selector: 'stock-counter',
+  providers:[COUNTER_CONTROL_ACCESOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
     <div class="stock-counter">
@@ -23,7 +31,11 @@ import {Component, Input} from '@angular/core';
 
    `
 })
-export class StockCounterComponent{
+export class StockCounterComponent implements ControlValueAccessor{
+
+  private onTouch: Function;
+  private onModelChange: Function;
+
   @Input()
   step: number = 10;
   @Input()
@@ -36,12 +48,28 @@ export class StockCounterComponent{
   increment() {
     if(this.value < this.max){
       this.value = this.value + this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
   }
 
   decrement() {
     if(this.value > this.min){
       this.value = this.value - this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
+  }
+
+  registerOnChange(fn): void {
+    this.onModelChange = fn;
+  }
+
+  registerOnTouched(fn): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(value): void {
+    this.value = value || 0;
   }
 }
--- profesional/04/forms-setup/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ profesional/04/forms-setup/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -16,12 +16,10 @@ import {Product} from '../../models/product.interface';
               {{ product.name}}
             </option>
           </select>
-          <input type="number" step="10" min="10" max="1000"
-                 formControlName="quantity">
           <stock-counter
             [step]="10"
             [min]="10"
-            [max]="1000"></stock-counter>
+            [max]="1000" formControlName="quantity"></stock-counter>
           <button type="button" (click)="onAdd()">
             Add Stock
           </button>
