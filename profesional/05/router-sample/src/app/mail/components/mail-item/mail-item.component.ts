1e99295ba40aa4236adcc1c674dca69546f0e225
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-28 20:19:19 -0500

    using navigation api

--- profesional/05/router-sample/src/app/mail/components/mail-item/mail-item.component.ts
+++ profesional/05/router-sample/src/app/mail/components/mail-item/mail-item.component.ts
@@ -1,14 +1,15 @@
 import {Component, Input} from '@angular/core';
 
 import {Mail} from '../../models/mail.interface';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
   template: `
     <a
-      class="mail-item" [routerLink]="['', { outlets: { pane: ['message', message.id] } }]"
-        routerLinkActive="active">
+      class="mail-item"
+      (click)="navigateToMessage()">
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date:'shortTime' }}</span>
@@ -20,4 +21,12 @@ import {Mail} from '../../models/mail.interface';
 export class MailItemComponent {
   @Input()
   message: Mail;
+
+  constructor(private router: Router) {
+  }
+
+  navigateToMessage(): void {
+    this.router.navigate(
+      ['', {outlets: {pane: ['message', this.message.id]}}]);
+  }
 }
